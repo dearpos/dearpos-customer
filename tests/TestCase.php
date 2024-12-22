fd27be911f97cb40d3b5e60bd0f633f6fed5fe65
Muhammad Irfan <mrfansi@outlook.com>
2024-12-22 23:53:41 +0700

    update

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,40 +1,10 @@
 <?php
 
-namespace DearPOS\DearPOSCustomer\Tests;
+namespace Tests;
 
-use DearPOS\DearPOSCustomer\DearPOSCustomerServiceProvider;
-use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Orchestra\Testbench\TestCase as Orchestra;
+use PHPUnit\Framework\TestCase as BaseTestCase;
 
-class TestCase extends Orchestra
+abstract class TestCase extends BaseTestCase
 {
-    use RefreshDatabase;
-
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'DearPOS\\DearPOSCustomer\\Database\\Factories\\'.class_basename($modelName).'Factory'
-        );
-    }
-
-    protected function getPackageProviders($app): array
-    {
-        return [
-            DearPOSCustomerServiceProvider::class,
-        ];
-    }
-
-    public function getEnvironmentSetUp($app): void
-    {
-        config()->set('database.default', 'testing');
-
-    }
-
-    protected function defineDatabaseMigrations(): void
-    {
-        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-    }
+    //
 }
